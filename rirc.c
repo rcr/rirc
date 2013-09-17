91810f157cccfd2d16d66acdbe0b0f5e145b6270
Richard Robbins <mail@robbinsr.com>
2013-09-16 21:31:09 -0400

    asynch i/o

--- rirc.c
+++ rirc.c
@@ -6,19 +6,21 @@
 
 /* init_ui(), cleaup() */
 /* move ui stuff to its own .c later */
+#include <poll.h>
 #include <termios.h>
 #include <string.h> /* memcpy */
 #include <sys/ioctl.h>
 struct winsize w;
 struct termios oterm, nterm;
 
+#define BUFFSIZE 512
 #define CLR "\033[H\033[J"
 
-void fatal(char *e) { perror(e); cleanup(0); exit(1); }
 void resize(int);
 void init_ui(void);
 void cleanup(int);
 void gui_loop(void);
+void fatal(char *e) { perror(e); cleanup(0); exit(1); }
 
 int
 main(int argc, char **argv)
@@ -68,19 +70,33 @@ cleanup(int clear)
 void
 gui_loop(void)
 {
-	char c;
-	int socket = -1;
+	char buf[BUFFSIZE];
+	int soc, count, ret, time = 200;
+	struct pollfd fds[2];
+
+	soc = connect_irc("localhost");
+
 	for (;;) {
-		if ((c = getchar()) == 'q')
-			break;
 
-		/* testing */
-		else if (c == 'c')
-			socket = connect_irc("localhost");
-		else if (c == 'd')
-			disconnect_irc(socket);
+		fds[0].fd = 0; /* stdin */
+		fds[0].events = POLLIN;
 
-		else
-			putchar(c);
+		fds[1].fd = soc;
+		fds[1].events = POLLIN;
+
+		ret = poll(fds, 2, time);
+
+		if (ret == 0) { /* timed out check input buffer */
+			time = 200;
+		} else if (fds[0].revents & POLLIN) {
+			count = read(0, buf, BUFFSIZE);
+			time = 0;
+			if (buf[0] == 'q')
+				break;
+		} else if (fds[1].revents & POLLIN) {
+			count = read(soc, buf, BUFFSIZE);
+			printf("%.*s", count, buf);
+			time = 0;
+		}
 	}
 }
