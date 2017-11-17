4fe24341c2c601bfd48cb35f19160bb9da18dcce
Richard Robbins <mail@rcr.io>
2017-11-17 15:21:12 -0800

    finish designing network interface

--- src/net2.h
+++ src/net2.h
@@ -3,32 +3,37 @@
 
 /* net.h
  *
+ * Network connection state
+ *
  * The state of a connection at any given time can be
  * described by one of the following:
  *
- *  - dx:   disconnected ~ Socket passively disconnected
+ *  - dxed: disconnected ~ Socket passively disconnected
  *  - cxng: connecting   ~ Socket connecting or in reconnect cycle
- *  - cx:   connected    ~ Socket connected
+ *  - cxed: connected    ~ Socket connected
  *  - ping: timing out   ~ Socket connected, network state in question
  *
- *     NEW            +----+
- *      |             |    |
- *      v             v    ^
- *  +------+ -----> +--------+
- *  |  dx  |        |  cxng  | <--+
- *  +------+ <----- +--------+    |
- *    ^  ^            |    ^      |
- *    |  |            |    |      |
- *    |  |            v    |      |
- *    |  |          +--------+    |
- *    |  +--------- |   cx   |    |
- *    |             +--------+    |
- *    |               |    ^      |
- *    |               |    |      |
- *    |               v    |      |
- *    |             +--------+    |
- *    +------------ |  ping  | ---+
- *                  +--------+
+ *     NEW             +----+
+ *      |              |    |
+ *      |              |   (F)
+ *      v              v    |
+ *  +------+ --(A)-> +--------+
+ *  | dxed |         |  cxng  | <--+
+ *  +------+ <-(B)-- +--------+    |
+ *    ^  ^             |    ^      |
+ *    |  |            (D)   |      |
+ *    |  |             |   (E)     |
+ *    |  |             v    |      |
+ *    |  |           +--------+    |
+ *    |  +-----(B)-- |  cxed  |    |
+ *    |              +--------+    |
+ *    |                |    ^      |
+ *    |               (G)   |      |
+ *    |                |   (D)     |
+ *    |                v    |     (E)
+ *    |              +--------+    |
+ *    +--------(B)-- |  ping  | ---+
+ *                   +--------+
  *
  * This module exposes functions for explicitly directing network
  * state as well declaring callback functions for state transitions
@@ -36,8 +41,16 @@
  *
  * The connection cycle is implements an exponential backoff routine
  *
- * Network state transitions result in callbacks:
- *     TODO: TBD
+ * Network state can be explicitly driven:
+ *   (A) net_cx:   enter connection/reconnection cycle
+ *   (B) net_dx:   close network connection
+ *   (C) net_free: free network connection
+ *
+ * Network state implicit transitions result in callbacks:
+ *   (D) on connection success:   net_cb_cxed
+ *   (E) on connection loss:      net_cb_dxed
+ *   (F) on reconnection attempt: net_cb_rxng
+ *   (G) on network timing out:   net_cb_ping
  *
  * Successful reads on stdin and connected sockets result in callbacks:
  *     from stdin:  net_cb_inp
@@ -45,15 +58,24 @@
  */
 
 struct connection;
-struct connection* connection(void*, const char*, const char*);
+
+struct connection* connection(
+		const char*,  /* host */
+		const char*,  /* port */
+		void const*); /* callback object */
 
 /* Explicit direction of net state */
-void net_cx(struct connection*);
-void net_dx(struct connection*);
+void net_cx  (struct connection*);
+void net_dx  (struct connection*);
+void net_free(struct connection*);
 void net_poll(void);
 
 /* Network callbacks */
 void net_cb_read_inp(void*);
 void net_cb_read_soc(void*);
+void net_cb_cxed(void*, const char*, ...);
+void net_cb_dxed(void*, const char*, ...);
+void net_cb_rxng(void*, const char*, ...);
+void net_cb_ping(void*);
 
 #endif
