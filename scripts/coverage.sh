dec9f0aa21ee68797fb4f1ebd5b5df4c4079aa2a
Richard Robbins <mail@rcr.io>
2018-11-30 00:55:31 -0800

    fix coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -7,9 +7,9 @@ rm -rf coverage
 mkdir coverage
 
 ENV=
-ENV+=" CC=\"gcc\""
-ENV+=" CC_EXT=\"-fprofile-arcs -ftest-coverage -fprofile-abs-path\""
-ENV+=" LD_EXT=\"-fprofile-arcs\""
+ENV="$ENV CC=\"gcc\""
+ENV="$ENV CC_EXT=\"-fprofile-arcs -ftest-coverage -fprofile-abs-path\""
+ENV="$ENV LD_EXT=\"-fprofile-arcs\""
 
 eval $ENV make clean debug test
 find . -name "*.gcno" -print0 | xargs -r0 gcov -prs $(pwd)/src 2>/dev/null
