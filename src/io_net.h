39b4210b6315cd44251f12b7791e5f728d83b8a8
Richard Robbins <mail@rcr.io>
2020-03-24 18:57:43 -0700

    add missing io_net files

---
+++ src/io_net.h
@@ -0,0 +1,20 @@
+#ifndef IO_NET_H
+#define IO_NET_H
+
+#include <stddef.h>
+
+enum io_net_err
+{
+	IO_NET_ERR_NONE,
+	IO_NET_ERR_SOCKET_FAILED,
+	IO_NET_ERR_UNKNOWN_HOST,
+	IO_NET_ERR_CONNECT_FAILED,
+	IO_NET_ERR_EINTR,
+	IO_NET_ERR_IP,
+	IO_NET_ERR_UNKNOWN
+};
+
+enum io_net_err io_net_connect(int*, const char*, const char*);
+enum io_net_err io_net_ip_str(int, char*, size_t);
+
+#endif
