acf4e43ad39a795030491b37fb568f93cae9a433
Richard Robbins <mail@rcr.io>
2020-09-09 23:48:29 -0700

    add test script for sanitizers

--- scripts/sanitizers_build.sh
+++ scripts/sanitizers_build.sh
@@ -4,14 +4,14 @@ set -e
 
 export CC=clang
 
-export CC_EXT="-fsanitize=address,undefined"
+export CC_EXT="-fsanitize=address,undefined -fno-omit-frame-pointer"
 export LD_EXT="-fsanitize=address,undefined"
 
 make -e clean debug
 
 mv rirc.debug rirc.debug.address
 
-export CC_EXT="-fsanitize=thread,undefined"
+export CC_EXT="-fsanitize=address,undefined -fno-omit-frame-pointer"
 export LD_EXT="-fsanitize=thread,undefined"
 
 make -e clean debug
