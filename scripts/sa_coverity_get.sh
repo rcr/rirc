597c7e2881af04c248e1206da8ca4c7601fc8e42
Richard Robbins <mail@rcr.io>
2021-05-07 23:45:11 -0700

    fix sonarscan script names, bump version

--- scripts/sa_coverity_get.sh
+++ scripts/sa_coverity_get.sh
@@ -24,6 +24,6 @@ echo "*" > "$DIR/.gitignore"
 curl -fsS https://scan.coverity.com/download/linux64 -o "$COVERITY_MD5" --data "token=$COVERITY_TOKEN&project=rcr%2Frirc&md5=1"
 curl -fsS https://scan.coverity.com/download/linux64 -o "$COVERITY_TGZ" --data "token=$COVERITY_TOKEN&project=rcr%2Frirc"
 
-printf "%s  %s" "$(cat "$COVERITY_MD5")" "$COVERITY_TGZ" | md5sum -c -
+printf "%s  %s" "$(cat "$COVERITY_MD5")" "$COVERITY_TGZ" | md5sum --quiet -c -
 
 tar -xzf "$COVERITY_TGZ" -C "$DIR" --strip-components 1
