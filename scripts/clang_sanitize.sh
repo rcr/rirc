f1a6da0f0054a50e746605a2c9d63f341cfb9625
Richard Robbins <mail@rcr.io>
2023-04-03 21:41:06 -0700

    fix clang sanitizers not each rebuilding mbedtls

--- scripts/clang_sanitize.sh
+++ scripts/clang_sanitize.sh
@@ -21,7 +21,7 @@ if [ -z "$BUILD" ] && [ -z "$CHECK" ]; then
 	exit
 fi
 
-CFLAGS_BASE="-pipe -O0 -g"
+CFLAGS_BASE="-pipe -O1 -g -fno-omit-frame-pointer"
 LDFLAGS_BASE="-pipe"
 
 export CC="clang"
@@ -29,39 +29,39 @@ export CFLAGS="$CFLAGS_BASE"
 export LDFLAGS="$LDFLAGS_BASE"
 export MAKEFLAGS="-e -f Makefile.dev -j $(nproc)"
 
-make clean-lib
-make libs
-
-export CFLAGS="$CFLAGS_BASE -fsanitize=address,undefined -fno-omit-frame-pointer"
+export CFLAGS="$CFLAGS_BASE -fsanitize=address,undefined"
 export LDFLAGS="$LDFLAGS_BASE -fsanitize=address,undefined"
 
 if [ ! -z "$BUILD" ]; then
-	make clean-dev
+	rm -f rirc.debug.address
+	make clean-dev clean-lib
 	make rirc.debug && mv rirc.debug rirc.debug.address
 fi
 
 if [ ! -z "$CHECK" ]; then
-	make clean-dev
+	make clean-dev clean-lib
 	make check
 fi
 
-export CFLAGS="$CFLAGS_BASE -fsanitize=memory,undefined -fno-omit-frame-pointer"
+export CFLAGS="$CFLAGS_BASE -fsanitize=memory,undefined"
 export LDFLAGS="$LDFLAGS_BASE -fsanitize=memory,undefined"
 
 if [ ! -z "$BUILD" ]; then
-	make clean-dev
+	rm -f rirc.debug.memory
+	make clean-dev clean-lib
 	make rirc.debug && mv rirc.debug rirc.debug.memory
 fi
 
 if [ ! -z "$CHECK" ]; then
-	make clean-dev
+	make clean-dev clean-lib
 	make check
 fi
 
-export CFLAGS="$CFLAGS_BASE -fsanitize=thread,undefined -fno-omit-frame-pointer"
+export CFLAGS="$CFLAGS_BASE -fsanitize=thread,undefined"
 export LDFLAGS="$LDFLAGS_BASE -fsanitize=thread,undefined"
 
 if [ ! -z "$BUILD" ]; then
-	make clean-dev
+	rm -f rirc.debug.thread
+	make clean-dev clean-lib
 	make rirc.debug && mv rirc.debug rirc.debug.thread
 fi
